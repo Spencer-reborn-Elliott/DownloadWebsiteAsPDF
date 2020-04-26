cf49acf77ba685ee55c76d4f86028398769597ab
rebornman <fm_reborn@hotmail.com>
2020-04-26 14:46:10 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace DownloadWebsiteAsPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxWebPage = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioPDF = new System.Windows.Forms.RadioButton();
+            this.radioPNG = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxWebPage
+            // 
+            this.textBoxWebPage.Location = new System.Drawing.Point(120, 25);
+            this.textBoxWebPage.Name = "textBoxWebPage";
+            this.textBoxWebPage.Size = new System.Drawing.Size(717, 20);
+            this.textBoxWebPage.TabIndex = 1;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(762, 162);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 4;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Single Web Page:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioPNG);
+            this.groupBox1.Controls.Add(this.radioPDF);
+            this.groupBox1.Location = new System.Drawing.Point(120, 64);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(156, 84);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Export File Format Options";
+            // 
+            // radioPDF
+            // 
+            this.radioPDF.AutoSize = true;
+            this.radioPDF.Checked = true;
+            this.radioPDF.Location = new System.Drawing.Point(16, 32);
+            this.radioPDF.Name = "radioPDF";
+            this.radioPDF.Size = new System.Drawing.Size(49, 17);
+            this.radioPDF.TabIndex = 0;
+            this.radioPDF.TabStop = true;
+            this.radioPDF.Text = ".PDF";
+            this.radioPDF.UseVisualStyleBackColor = true;
+            // 
+            // radioPNG
+            // 
+            this.radioPNG.AutoSize = true;
+            this.radioPNG.Location = new System.Drawing.Point(16, 55);
+            this.radioPNG.Name = "radioPNG";
+            this.radioPNG.Size = new System.Drawing.Size(51, 17);
+            this.radioPNG.TabIndex = 1;
+            this.radioPNG.Text = ".PNG";
+            this.radioPNG.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 204);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.textBoxWebPage);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Download Website As PDF";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxWebPage;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioPNG;
+        private System.Windows.Forms.RadioButton radioPDF;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,212 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace DownloadWebsiteAsPDF
+{
+    public partial class Form1 : Form
+    {
+        string Title = "Download Website As PDF";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            if (textBoxWebPage.Text.Length > 0)
+            {
+                DownloadWebPage(textBoxWebPage.Text);
+            }
+            else
+            {
+                MessageBox.Show("You need to enter a web page or sitemap address in the text box before trying to download.", Title);
+            }
+        }
+        
+        
+        
+        void DownloadWebPage(string url)
+        {
+            try
+            {
+                WebBrowser browser = new WebBrowser();
+                browser.ScriptErrorsSuppressed = true;
+                browser.ScrollBarsEnabled = false;
+                browser.AllowNavigation = true;
+
+                browser.ScriptErrorsSuppressed = true;
+
+                browser.Navigate(url);
+                browser.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(browser_DocumentCompleted);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Title);
+            }
+        }
+
+        void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            try
+            {
+                WebBrowser browser = sender as WebBrowser;
+                if (browser.ReadyState != WebBrowserReadyState.Complete)
+                {
+                    return;
+                }
+                browser.Size = browser.Document.Body.ScrollRectangle.Size;
+                browser.Width = Screen.PrimaryScreen.Bounds.Width;
+                using (var bitmap = new Bitmap(browser.Width, browser.Height))
+                {
+                    browser.DrawToBitmap(bitmap, new Rectangle(0, 0, browser.Width, browser.Height));
+                    Image image = (Image)bitmap.Clone();
+                    if (radioPNG.Checked == true)
+                    {
+                        string filename = RemoveIllegalFileNameChars(browser.Url.ToString(), "") + ".png";
+                        image.Save(filename);
+                    }
+                    else if (radioPDF.Checked == true)
+                    {
+                        MemoryStream strm = new MemoryStream();
+                        image.Save(strm, System.Drawing.Imaging.ImageFormat.Png);
+
+                        XImage xImage = XImage.FromStream(strm);
+
+                        PdfDocument document = new PdfDocument();
+                        PdfPage page = document.AddPage();
+                        page.Width = browser.Width;
+                        page.Height = browser.Height;
+
+                        document.Options.NoCompression = true;
+                        XGraphics gfx = XGraphics.FromPdfPage(page);
+                        gfx.DrawImage(xImage, new XRect(0, 0, page.Width, page.Height));
+                        string filename = RemoveIllegalFileNameChars(browser.Url.ToString(), "") + ".pdf";
+                        page.Close();
+                        document.Save(filename);
+                        xImage.Dispose();
+                        strm.Close();
+                        document.Dispose();
+                    }
+                    image.Dispose();
+                    bitmap.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Title);
+            }
+        }
+
+
+
+        public static string RemoveIllegalFileNameChars(string input, string replacement = "")
+        {
+            var regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            var r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+            return r.Replace(input, replacement);
+        }
+
+
+
+
+
+
+
+
+
+        /*
+
+        void Download(string url)
+        {
+            WebBrowser browser = new WebBrowser();
+            browser.ScriptErrorsSuppressed = true;
+
+            browser.Navigate(url);
+            browser.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(browser_DocumentCompleted);
+        }
+
+        void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            WebBrowser browser = sender as WebBrowser;
+            if (browser.ReadyState != WebBrowserReadyState.Complete)
+            {
+                return;
+            }
+
+
+            HtmlElementCollection TheImages = browser.Document.GetElementsByTagName("img");
+            System.Net.WebClient wClient = new System.Net.WebClient();
+
+            char[] filetype = new char[4];
+            string gtype;
+
+            for (int i = 0; i < TheImages.Count; i++)
+            {
+                try
+                {
+                    TheImages[i].GetAttribute("src").CopyTo(TheImages[i].GetAttribute("src").Length - 4, filetype, 0, 4);
+                    gtype = new string(filetype);
+                    wClient.DownloadFile(TheImages[i].GetAttribute("src"), i.ToString() + gtype);
+                    LogIt(TheImages[i].GetAttribute("src"));
+                }
+                catch (Exception ex)
+                {
+                    LogIt(ex.Message.ToString());
+                }
+            }
+        }
+
+
+
+
+
+        void Download(string url)
+        {
+            HtmlToPdf converter = new SelectPdf.HtmlToPdf();
+            converter.Options.RenderingEngine = RenderingEngine.WebKit;
+            converter.Options.CssMediaType = HtmlToPdfCssMediaType.Screen;
+            converter.Options.DisplayCutText = true;
+            converter.Options.DisplayFooter = true;
+            converter.Options.DisplayHeader = true;
+            converter.Options.DrawBackground = true;
+            converter.Options.EmbedFonts = true;
+            converter.Options.ExternalLinksEnabled = true;
+            converter.Options.InternalLinksEnabled = true;
+            converter.Options.JavaScriptEnabled = true;
+            converter.Options.JpegCompressionEnabled = true;
+            converter.Options.KeepImagesTogether = false;
+            converter.Options.PdfCompressionLevel = PdfCompressionLevel.Normal;
+            converter.Options.PluginsEnabled = true;
+
+
+            PdfDocument doc = converter.ConvertUrl(textBoxURL.Text);
+
+            doc.Save("test.pdf");
+            doc.Close();
+        }
+        */
+
+
+
+
+
+
+
+
+
+
+    }
+}
